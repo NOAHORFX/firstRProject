a59bd20b65eb0bfcd3b9128da4f1262500ba4f45
Noah Feng <2027283767@qq.com>
2024-09-17 23:43:10 +0100

    Add files via upload

---
+++ Assignment1.R
@@ -0,0 +1,21 @@
+# Task 1
+#Step 1
+animals <- c("Snake", "Ostrich", "Cat", "Spider")
+animals
+#Step 2
+num_legs <- c(0, 2, 4, 8)
+num_legs
+#Step 3
+animals_df <- data.frame(animals, num_legs)
+animals_df
+
+# Task 2
+#Step 1
+ls()
+?ls
+?rm
+rm(list = ls())
+ls()
+
+# Task 3
+
